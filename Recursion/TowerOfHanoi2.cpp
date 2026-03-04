b2651626d71d41644ffe7f2e327d90daa147acbb
Muhammad Umar Shehzad <mumarshehzad479@gmail.com>
2026-03-04 21:31:20 +0500

    Feat: Added file for Tower Of Hanoi 2

---
+++ Recursion/TowerOfHanoi2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+void tower (int,char,char,char);
+int main()
+{
+	int n;
+	cout<<"Enter a number : ";cin>>n;
+	tower(n,'A','B','C');
+	
+return 0;
+}
+
+
+void tower (int n,char s, char h, char d)
+{
+	if (n==0)
+	{
+	return;
+	}
+	
+	tower(n-1,s,d,h);
+	cout<<s<<" to "<< d<<endl;
+	tower(n-1,h,s,d);
+	
+return;
+}
